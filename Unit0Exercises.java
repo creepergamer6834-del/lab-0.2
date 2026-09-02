21ac99f640f22d0bfe3b195ee57a6395e962e4f8
Sebastian L <creepergamer6834@gmail.com>
2026-09-02 13:29:45 -0700

    fixed

--- Unit0Exercises.java
+++ Unit0Exercises.java
@@ -6,20 +6,38 @@ public class Unit0Exercises {
         int score = 85;
         double price = 19.99;
         boolean isPassing = true;
-        System.out.println("Name\tInitial Value\nScore:\t" + score + "\nDouble:\t" + price
-                + "\nBoolean:" + isPassing);
+        final double TAX_RATE = 0.0925;
+        char letter = 'A';
+        long population = 8000000000L;
+
+        System.out.println("Name\tInitial Value");
+        System.out.println("Score: " + score);
+        System.out.println("Double: " + price);
+        System.out.println("Boolean: " + isPassing);
+        System.out.println("Letter: " + letter);
+        System.out.println("Population: " + population);
+        System.out.println("Tax Rate: " + TAX_RATE);
 
         // TODO: Print the original values using the required labels.
 
         System.out.println("--- After Updates ---");
 
         // TODO: Reassign score, price, and isPassing to their updated values.
+
         score += 5;
         price += 2.50;
         isPassing = false;
+        letter = 'A';
+        population = 8000000000L;
 
         // TODO: Print the updated values using the required labels.
-        System.out.println("Name\tInitial Value\nScore:\t" + score + "\nDouble:\t" + price
-                + "\nBoolean:" + isPassing);
+
+        System.out.println("\nName\tInitial Value");
+        System.out.println("Score: " + score);
+        System.out.println("Double: " + price);
+        System.out.println("Boolean: " + isPassing);
+        System.out.println("Letter: " + letter);
+        System.out.println("Population: " + population);
+        System.out.println("Tax Rate: " + TAX_RATE);
     }
 }
