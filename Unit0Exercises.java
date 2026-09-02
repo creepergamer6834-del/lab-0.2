73f1240993219892819cbb29c37de01f6e4fbb70
Sebastian L <creepergamer6834@gmail.com>
2026-09-02 13:20:23 -0700

    a

--- Unit0Exercises.java
+++ Unit0Exercises.java
@@ -6,11 +6,8 @@ public class Unit0Exercises {
         int score = 85;
         double price = 19.99;
         boolean isPassing = true;
-        final double TAX_RATE = 0.0925;
-        char letter = 'A';
-        long population = 8000000000L;
         System.out.println("Name\tInitial Value\nScore:\t" + score + "\nDouble:\t" + price
-                + "\nBoolean:" + isPassing + "\nTax Rate\t" + TAX_RATE + "\nLetter\t" + letter "\nPopulation\t" + population);
+                + "\nBoolean:" + isPassing);
 
         // TODO: Print the original values using the required labels.
 
@@ -20,12 +17,9 @@ public class Unit0Exercises {
         score += 5;
         price += 2.50;
         isPassing = false;
-        TAX_RATE = 0.0926;
-        letter = "A";
-        population = 8000000000;
-    
+
         // TODO: Print the updated values using the required labels.
         System.out.println("Name\tInitial Value\nScore:\t" + score + "\nDouble:\t" + price
-                + "\nBoolean:" + isPassing + "\nTax Rate\t" + TAX_RATE + "\nLetter\t" + letter "\nPopulation\t" + population);
+                + "\nBoolean:" + isPassing);
     }
 }
